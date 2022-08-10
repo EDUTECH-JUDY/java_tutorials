77c0657513afae3af2c39b79bc884ec85689197a
오세영 <@DESKTOP-QHM5UVS>
2022-08-10 17:52:00 +0900

    API와 API문서

---
+++ src/org/opentutorials/javatutorials/library/LibraryDemo1.java
@@ -0,0 +1,14 @@
+package org.opentutorials.javatutorials.library;
+
+//패키지 java.lang은 자바 프로그래밍을 하기 위해서 필수적인 클래스들을 모아둔 패키지다. 
+//따라서 사용자의 편의를 위해서 자동으로 로딩을 하고 있는 것이다.
+import java.lang.*;
+
+public class LibraryDemo1 {
+
+	public static void main(String[] args) {
+//		클래스 System은 바로 이 java.lang의 소속이다.
+		System.out.println(1);
+	}
+
+}
